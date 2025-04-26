8f9a7d4dae9462a9978aaf9092cd253a071c275f
Didelfo <114946228+Didelfo@users.noreply.github.com>
2025-04-26 21:25:03 +0200

    inicial

---
+++ main.js
@@ -0,0 +1,10 @@
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => section.classList.remove('active'));
+    const selectedSection = document.getElementById(sectionId);
+    selectedSection.classList.add('active');
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    showSection('sobre-mi');
+});
